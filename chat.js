992487e0a61a89bfa8a81b8d3ab71e1b3615c631
SiddharthB70 <siddharth7002b@gmail.com>
2022-12-22 22:19:56 +0530

    Initial Commit

---
+++ chat.js
@@ -0,0 +1,42 @@
+const ws = new WebSocket("ws://localhost:8001/");
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const chatWindow = document.querySelector('.chat-window')
+// const sender = document.getElementById("sender");
+// const receiver = document.getElementById("receiver");
+
+
+button.addEventListener("click",sendMessage);
+document.addEventListener("keydown",(e)=>{
+    if(e.key == 'Enter' && document.activeElement == input){
+        sendMessage();
+    }
+})
+ws.addEventListener("message",({data})=>{
+    addMessage(data,"receiver");
+})
+
+function sendMessage(){
+    if(input.value == '')
+        return;
+    ws.send(JSON.stringify({type: "data",value: `${input.value}`}))
+    addMessage(input.value,"sender")
+    input.value = "";
+}
+
+function addMessage(data,messageType){
+    const message = document.createElement('p');
+    message.classList.add("message");
+    message.textContent = data;
+    if(messageType == "receiver")
+        message.classList.add("receiver");
+    chatWindow.appendChild(message);
+}
+
+window.onload = () => {
+    ws.addEventListener("open",()=>{
+        const event = {type: "connection",value:"ok"};
+        ws.send(JSON.stringify(event));
+    })
+    input.value = "";
+}
---
+++ server.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+import json
+
+clients = []
+
+async def handler(websocket):
+    async for message in websocket:
+        message = json.loads(message)
+        if message['type'] == "connection":
+            if websocket not in clients:
+                clients.append(websocket)
+        else:
+            broadClients = clients.copy()
+            broadClients.remove(websocket)
+            websockets.broadcast(broadClients,message['value'])
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
