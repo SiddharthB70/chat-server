3da5a587a98f65fb645c89f82b9c1a8079219c71
SiddharthB70 <siddharth7002b@gmail.com>
2022-12-22 22:33:09 +0530

    Changed Socket Server

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("ws://localhost:8001/");
+const ws = new WebSocket("https://pbl-socketserver.onrender.com");
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const chatWindow = document.querySelector('.chat-window')
